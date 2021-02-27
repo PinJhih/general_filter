1a84554c8f8b47fde7a7d240db367abcc65a57e2
PinJhih <isaacwang29@gmail.com>
2021-02-27 20:55:48 +0800

    feat: 讀取字串資源

---
+++ app/src/main/java/com/example/general_filter/Department.kt
@@ -0,0 +1,18 @@
+package com.example.general_filter
+
+data class Department(
+    var schoolName: String = "",
+    var departmentName: String = "",
+    var code: String = "",
+    var enrollmentQuota: String = "",
+    var genderRequirement: String = "",
+    var examQuota: String = "",
+    var aboriginalQuota: String = "",
+    var offshoreIslandsQuota: String = "",
+    var planQuota: String = "",
+    var registrationFee: String = "",
+    var examDate: String = "",
+    var category: String = "",
+    var technicalExam: String = "",
+    var helpingMeasures: String = ""
+)
--- app/src/main/java/com/example/general_filter/MainActivity.kt
+++ app/src/main/java/com/example/general_filter/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.general_filter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val departments = ArrayList<Department>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val ids = arrayOf(
+            R.array.school_name, R.array.department_name,
+            R.array.code, R.array.exam_date,
+            R.array.exam_quota, R.array.enrollment_quota
+        )
+        val res = arrayListOf<Array<String>>()
+        for (i in ids)
+            res.add(resources.getStringArray(i))
+        for (i in res[0].indices) {
+            val department = Department()
+            department.schoolName = res[0][i]
+            department.departmentName = res[1][i]
+            department.code = res[2][i]
+            department.examDate = res[3][i]
+            department.examQuota = res[4][i]
+            department.enrollmentQuota = res[5][i]
+            departments.add(department)
+        }
     }
 }
