a5c38d0b9f860695e354759ebe2d716b5ee06cee
PinJhih <isaacwang29@gmail.com>
2021-04-05 21:19:52 +0800

    fix: 修正索引錯誤

--- app/src/main/java/com/example/general_filter/WebActivity.kt
+++ app/src/main/java/com/example/general_filter/WebActivity.kt
@@ -8,28 +8,28 @@ import java.util.ArrayList
 class WebActivity : AppCompatActivity() {
 
     private var codes = ArrayList<String>()
+    private var index = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
         wv_result.settings.useWideViewPort = true
-        var index = 0
         intent.extras?.let {
             codes = it.getStringArrayList("codes")!!
-            if (codes.count() != 0)
+            if (codes.size != 0)
                 wv_result.loadUrl(getUrl(codes[0]))
         }
         setWebView(0)
         btn_last.setOnClickListener {
             index--
-            if (index == 0)
+            if (index < 0)
                 index = 0
             setWebView(index)
         }
         btn_next.setOnClickListener {
             index++
-            if (index == codes.size)
-                index--
+            if (index >= codes.size)
+                index = codes.size - 1
             setWebView(index)
         }
     }
@@ -38,7 +38,7 @@ class WebActivity : AppCompatActivity() {
         "https://www.cac.edu.tw/apply110/system/110_aColQry4qy_forapply_o5wp6ju/html/110_$code.htm?v=1.0"
 
     private fun setWebView(index: Int) {
-        if (codes.count() != 0) {
+        if (codes.size != 0) {
             wv_result.loadUrl(getUrl(codes[index]))
             title = codes[index]
         }
