2b91ba5c3ce39ebf48313a574ba3a2d3960928bd
PinJhih <isaacwang29@gmail.com>
2021-04-28 09:34:33 +0800

    feat: 加入廣告

--- app/src/main/java/com/example/general_filter/MainActivity.kt
+++ app/src/main/java/com/example/general_filter/MainActivity.kt
@@ -9,6 +9,8 @@ import android.text.Editable
 import android.text.TextWatcher
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +23,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MobileAds.initialize(this) {}
+        val adRequest = AdRequest.Builder().build()
+        ad_main.loadAd(adRequest)
+
         for (i in 0 until 2181)
             status += 'f'
         userInfo = getSharedPreferences("userInfo", Activity.MODE_PRIVATE)
